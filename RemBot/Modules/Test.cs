92cf16851f2eecc4dfe9a175c5c5f75a330ba3ce
Omegalomax <30303540+Omegalomax@users.noreply.github.com>
2017-07-19 20:59:10 -0400

    Update Test.cs

--- RemBot/Modules/Test.cs
+++ RemBot/Modules/Test.cs
@@ -42,6 +42,9 @@ namespace RemBot.Modules
             }
         }
 
+
+
+
         [Command("hello")]
         public async Task hello2()
         {
@@ -74,7 +77,10 @@ namespace RemBot.Modules
                 await Context.Channel.SendMessageAsync(Globals.greetings0[rnd.Next(Globals.greetings0.Count)]);
             }
         }
-        
+
+
+
+
         [Command("hug")]
         public async Task hug1()
         {
@@ -108,6 +114,257 @@ namespace RemBot.Modules
             }
         }
         
+        
+        
+        
+        [Command("joke")]
+        public async Task joke1()
+        {
+            if (Globals.affectionMeter.ContainsKey(Context.User.Username))
+            {
+                int RemLove = Globals.affectionMeter[Context.User.Username];
+                if (RemLove < 25)
+                {
+                    await Context.Channel.SendMessageAsync(Globals.joke0[rnd.Next(Globals.joke0.Count)]);
+                }
+                else if (RemLove < 50)
+                {
+                    await Context.Channel.SendMessageAsync(Globals.joke1[rnd.Next(Globals.joke1.Count)]);
+                }
+                else if (RemLove < 100)
+                {
+                    await Context.Channel.SendMessageAsync(Globals.joke2[rnd.Next(Globals.joke2.Count)]);
+                }
+                else if (RemLove < 200)
+                {
+                    await Context.Channel.SendMessageAsync(Globals.joke3[rnd.Next(Globals.joke3.Count)]);
+                }
+                else
+                {
+                    await Context.Channel.SendMessageAsync(Globals.joke4[rnd.Next(Globals.joke4.Count)]);
+                }
+            }
+            else
+            {
+                await Context.Channel.SendMessageAsync(Globals.joke0[rnd.Next(Globals.joke0.Count)]);
+            }
+        }
+		
+
+		
+		
+[Command("compliment")]
+        public async Task compliment1()
+        {
+            if (Globals.affectionMeter.ContainsKey(Context.User.Username))
+            {
+                int RemLove = Globals.affectionMeter[Context.User.Username];
+                if (RemLove < 25)
+                {
+                    await Context.Channel.SendMessageAsync(Globals.compliment0[rnd.Next(Globals.compliment0.Count)]);
+                }
+                else if (RemLove < 50)
+                {
+                    await Context.Channel.SendMessageAsync(Globals.compliment1[rnd.Next(Globals.compliment1.Count)]);
+                }
+                else if (RemLove < 100)
+                {
+                    await Context.Channel.SendMessageAsync(Globals.compliment2[rnd.Next(Globals.compliment2.Count)]);
+                }
+                else if (RemLove < 200)
+                {
+                    await Context.Channel.SendMessageAsync(Globals.compliment3[rnd.Next(Globals.compliment3.Count)]);
+                }
+                else
+                {
+                    await Context.Channel.SendMessageAsync(Globals.compliment4[rnd.Next(Globals.compliment4.Count)]);
+                }
+            }
+            else
+            {
+                await Context.Channel.SendMessageAsync(Globals.compliment0[rnd.Next(Globals.compliment0.Count)]);
+            }
+        }
+		
+		
+		
+		
+[Command("pet")]
+        public async Task pet1()
+        {
+            if (Globals.affectionMeter.ContainsKey(Context.User.Username))
+            {
+                int RemLove = Globals.affectionMeter[Context.User.Username];
+                if (RemLove < 25)
+                {
+                    await Context.Channel.SendMessageAsync(Globals.pet0[rnd.Next(Globals.pet0.Count)]);
+                }
+                else if (RemLove < 50)
+                {
+                    await Context.Channel.SendMessageAsync(Globals.pet1[rnd.Next(Globals.pet1.Count)]);
+                }
+                else if (RemLove < 100)
+                {
+                    await Context.Channel.SendMessageAsync(Globals.pet2[rnd.Next(Globals.pet2.Count)]);
+                }
+                else if (RemLove < 200)
+                {
+                    await Context.Channel.SendMessageAsync(Globals.pet3[rnd.Next(Globals.pet3.Count)]);
+                }
+                else
+                {
+                    await Context.Channel.SendMessageAsync(Globals.pet4[rnd.Next(Globals.pet4.Count)]);
+                }
+            }
+            else
+            {
+                await Context.Channel.SendMessageAsync(Globals.pet0[rnd.Next(Globals.pet0.Count)]);
+            }
+        }		
+		
+		
+		
+		
+[Command("pat")]
+        public async Task pet1()
+        {
+            if (Globals.affectionMeter.ContainsKey(Context.User.Username))
+            {
+                int RemLove = Globals.affectionMeter[Context.User.Username];
+                if (RemLove < 25)
+                {
+                    await Context.Channel.SendMessageAsync(Globals.pet0[rnd.Next(Globals.pet0.Count)]);
+                }
+                else if (RemLove < 50)
+                {
+                    await Context.Channel.SendMessageAsync(Globals.pet1[rnd.Next(Globals.pet1.Count)]);
+                }
+                else if (RemLove < 100)
+                {
+                    await Context.Channel.SendMessageAsync(Globals.pet2[rnd.Next(Globals.pet2.Count)]);
+                }
+                else if (RemLove < 200)
+                {
+                    await Context.Channel.SendMessageAsync(Globals.pet3[rnd.Next(Globals.pet3.Count)]);
+                }
+                else
+                {
+                    await Context.Channel.SendMessageAsync(Globals.pet4[rnd.Next(Globals.pet4.Count)]);
+                }
+            }
+            else
+            {
+                await Context.Channel.SendMessageAsync(Globals.pet0[rnd.Next(Globals.pet0.Count)]);
+            }
+        }
+		
+		
+		
+		
+[Command("kiss")]
+        public async Task kiss1()
+        {
+            if (Globals.affectionMeter.ContainsKey(Context.User.Username))
+            {
+                int RemLove = Globals.affectionMeter[Context.User.Username];
+                if (RemLove < 25)
+                {
+                    await Context.Channel.SendMessageAsync(Globals.kiss0[rnd.Next(Globals.kiss0.Count)]);
+                }
+                else if (RemLove < 50)
+                {
+                    await Context.Channel.SendMessageAsync(Globals.kiss1[rnd.Next(Globals.kiss1.Count)]);
+                }
+                else if (RemLove < 100)
+                {
+                    await Context.Channel.SendMessageAsync(Globals.kiss2[rnd.Next(Globals.kiss2.Count)]);
+                }
+                else if (RemLove < 200)
+                {
+                    await Context.Channel.SendMessageAsync(Globals.kiss3[rnd.Next(Globals.kiss3.Count)]);
+                }
+                else
+                {
+                    await Context.Channel.SendMessageAsync(Globals.kiss4[rnd.Next(Globals.kiss4.Count)]);
+                }
+            }
+            else
+            {
+                await Context.Channel.SendMessageAsync(Globals.kiss0[rnd.Next(Globals.kiss0.Count)]);
+            }
+        }
+		
+		
+		
+		
+[Command("wave")]
+        public async Task wave1()
+        {
+            if (Globals.affectionMeter.ContainsKey(Context.User.Username))
+            {
+                int RemLove = Globals.affectionMeter[Context.User.Username];
+                if (RemLove < 25)
+                {
+                    await Context.Channel.SendMessageAsync(Globals.wave0[rnd.Next(Globals.wave0.Count)]);
+                }
+                else if (RemLove < 50)
+                {
+                    await Context.Channel.SendMessageAsync(Globals.wave1[rnd.Next(Globals.wave1.Count)]);
+                }
+                else if (RemLove < 100)
+                {
+                    await Context.Channel.SendMessageAsync(Globals.wave2[rnd.Next(Globals.wave2.Count)]);
+                }
+                else if (RemLove < 200)
+                {
+                    await Context.Channel.SendMessageAsync(Globals.wave3[rnd.Next(Globals.wave3.Count)]);
+                }
+                else
+                {
+                    await Context.Channel.SendMessageAsync(Globals.wave4[rnd.Next(Globals.wave4.Count)]);
+                }
+            }
+            else
+            {
+                await Context.Channel.SendMessageAsync(Globals.wave0[rnd.Next(Globals.wave0.Count)]);
+            }
+        }
+				
+		
+	
+		
+[Command("gift")]
+        public async Task gift1()
+        {
+            if (Globals.affectionMeter.ContainsKey(Context.User.Username))
+            {
+                int RemLove = Globals.affectionMeter[Context.User.Username];
+                if (RemLove < 25)
+                {
+                    await Context.Channel.SendMessageAsync(Globals.gift0[rnd.Next(Globals.gift0.Count)]);
+                }
+                else if (RemLove < 50)
+                {
+                    await Context.Channel.SendMessageAsync(Globals.gift1[rnd.Next(Globals.gift1.Count)]);
+                }
+                else if (RemLove < 100)
+                {
+                    await Context.Channel.SendMessageAsync(Globals.gift2[rnd.Next(Globals.gift2.Count)]);
+                }
+                else if (RemLove < 200)
+                {
+                    await Context.Channel.SendMessageAsync(Globals.gift3[rnd.Next(Globals.gift3.Count)]);
+                }
+                else
+                {
+                    await Context.Channel.SendMessageAsync(Globals.gift4[rnd.Next(Globals.gift4.Count)]);
+                }
+            }
+            else
+            {
+                await Context.Channel.SendMessageAsync(Globals.gift0[rnd.Next(Globals.gift0.Count)]);
+            }
+        }
         [Command("eromanga")]
         public async Task ero1()
         {
